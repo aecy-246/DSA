7e68d467a57069bba91bba87d80f862ef52910e2
Aksh Chauhan <aecy.246@gmail.com>
2026-08-05 23:24:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.45%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0;
+        int j=0;
+        string ans="";
+        while(i<word1.size() && j<word2.size()){
+            if(i==j){
+                ans+=word1[i];
+                i++;
+            }
+            else{
+                ans+=word2[j];
+                j++;
+            }
+        }
+        while(i<word1.size()){
+            ans+=word1[i];
+            i++;
+        }
+        while(j<word2.size()){
+            ans+=word2[j];
+            j++;
+        }
+        return ans;
+    }
+};
