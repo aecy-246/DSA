f36ea4fd12f1ea48f40725d1237eb4066a5e5163
Aksh Chauhan <aecy.246@gmail.com>
2026-08-31 11:37:06 +0530

    Time: 0 ms (100%), Space: 28.4 MB (14.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int temp = 0;
-        int flag = 0;
-        int answer;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1]){
-                flag=1;
-            }
-        }
-        if(flag==0){
-            return nums[0];
-        }
+        int ans;
+        unordered_map<int,int> umap;
         for(int i=0;i<nums.size();i++){
-            for(int j=i;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    temp++;
-                }
-            }
-            if(temp>nums.size()/2){
-                answer = nums[i];
-            }
-            temp=0;
+            umap[nums[i]]++;
         }
-        return answer;
+        for(auto x:umap){
+            if(x.second>=((nums.size()+1)/2)){
+                ans= x.first;
+            }
+        }
+        return ans;
     }
 };
