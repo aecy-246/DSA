f19b34212505a6898d2182d783a5c52c86fbf7a3
Aksh Chauhan <aecy.246@gmail.com>
2026-09-10 18:43:22 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        // Kadane's algorithm
         long long max = LONG_MIN;
         long long sum = 0;
         for(int i=0;i<nums.size();i++){
