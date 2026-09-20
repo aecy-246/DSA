ab986bcdadecf7800931ad17a930afca2cabab5f
Aksh Chauhan <aecy.246@gmail.com>
2026-09-21 00:41:34 +0530

    Time: 0 ms (100%), Space: 15.7 MB (82.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        //STL Solution
-        next_permutation(nums.begin(),nums.end());
-        return;
+        int n = nums.size();
+        int pos = -1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pos = i;
+                break;
+            }
+        }
+        if(pos==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i=n-1;i>pos;i--){
+            if(nums[i]>nums[pos]){
+                int temp = nums[i];
+                nums[i] = nums[pos];
+                nums[pos] = temp;
+                break;
+            }
+        }
+        reverse(nums.begin()+pos+1,nums.end());
     }
 };
